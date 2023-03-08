3aa162ecb41616392160f74f84e739bfa7ab65ef
Hareem-Saad <hareemsaadqsols@gmail.com>
2023-03-08 20:20:55 +0500

    added tests

--- test/foundry/test.t.sol
+++ test/foundry/test.t.sol
@@ -13,15 +13,18 @@ contract StableCoinTest is Test {
     address user2 = address(3);
 
     function setUp() public {
-        vm.deal(owner, 2 ether);
-        vm.deal(user1, 2 ether);
+        // vm.deal(owner, 2 ether);
+        vm.deal(user1, 20 ether);
         vm.deal(user2, 2 ether);
         vm.prank(owner);
         addr = new CollateralizedStablecoin();
     }
 
     function testDepositCollateral(uint256 amount) public {
-        vm.assume(amount > 0 && amount < 500);
+        // fuzz.max_test_rejects = 1000000
+        // vm.assume(amount > 0 && amount < 500);
+        amount = bound(amount, 1, 1000);
+
         // uint256 amount = 100;
         vm.startPrank(user1);
         uint256 price = addr.getExchangeRate(amount);
@@ -46,5 +49,42 @@ contract StableCoinTest is Test {
 
         assertEq (addr.balanceOf(user1), prevBalance - (amount * 10 ** 18));
         assertEq (address(addr).balance, prevContractBalance - redeemed);
+    }
+
+    function testWithdrawCollateral(uint256 amount) public {
+        // vm.assume(amount > 0 && amount <= 1000);
+        amount = bound(amount, 1, 1000);
+        // require(amount >= 100 && amount <= 1000);
+
+        testDepositCollateral(1000);
+
+        uint256 prevBalance = addr.balanceOf(user1);
+        uint256 prevContractBalance = address(addr).balance;
+        // uint256 amount = 50
+        vm.startPrank(user1);
+        (uint256 redeemed, ) = addr.calculatePriceForSale(amount);
+        addr.withdrawCollateral(amount);
+        vm.stopPrank();
+
+        assertEq (addr.balanceOf(user1), prevBalance - (amount * 10 ** 18));
+        assertEq (address(addr).balance, prevContractBalance - redeemed);
+    }
+
+    function testWithdrawTax() public {
+        uint256 amount = 1000;
+
+        vm.startPrank(user1);
+        uint256 price = addr.getExchangeRate(amount);
+        addr.depositCollateral{value: price}(amount);
+
+        (, uint256 tax) = addr.calculatePriceForSale(amount);
+        addr.withdrawCollateral(amount);
+        vm.stopPrank();
+
+        vm.prank(owner);
+        addr.withdrawTax();
+
+        assertEq(address(owner).balance, tax);
+
     }
 }
